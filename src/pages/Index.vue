6b77b4e403291bf1992ec729dff1825f6a0f470a
yllhwa <yll2002mail@gmail.com>
2022-04-08 20:19:08 +0800

    misc: 调整部分Option API为Composition API

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,25 +2,22 @@
 import Header from "../components/Header.vue";
 import Canvas from "../components/Canvas.vue";
 import Pannel from "../components/Pannel.vue";
+import { ref } from "vue";
+
+let drawColor = ref("rgb(255, 255, 255)");
+
+let colorClick = (color) => {
+  drawColor.value = color;
+};
 </script>
 
 <template>
   <Header />
   <Canvas :drawColor="drawColor" />
-  <Pannel class="left-0 right-0 absolute bottom-0" @colorClick="colorClick" />
+  <Pannel
+    :drawColor="drawColor"
+    class="left-0 right-0 absolute bottom-0"
+    @colorClick="colorClick"
+  />
 </template>
-<script>
-export default {
-  data() {
-    return {
-      drawColor: "rgb(255, 255, 255)",
-    };
-  },
-  methods: {
-    colorClick(color) {
-      this.drawColor = color;
-    },
-  },
-};
-</script>
 <style></style>
