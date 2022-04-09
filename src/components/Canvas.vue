05a23cbcf114c3ee1120eeccedd8ec796520a07f
yllhwa <yll2002mail@gmail.com>
2022-04-09 09:14:25 +0800

    将canvas.vue切换为组合式API

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,13 +1,81 @@
 <script setup>
 import axios from "axios";
-// import socketio
 import io from "socket.io-client";
 import Utils from "../utils/utils.js";
 import canvasController from "../utils/canvasController.js";
-// get prop color
-defineProps({
+import { ref, unref, onMounted } from "@vue/runtime-core";
+
+const props = defineProps({
   drawColor: String,
 });
+
+// 地图大小
+const size = { width: 1000, height: 1000 };
+// canvas DOM元素
+const canvas = ref(null);
+// 控制缩放、移动
+let _canvasController = ref(new canvasController());
+const ctx = ref(null);
+// websocket
+const socket = ref(null);
+
+const drawPoint = (ctx, x, y, color) => {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, 1, 1);
+};
+
+onMounted(() => {
+  const initCanvas = (canvas) => {
+    let ctx = canvas.getContext("2d");
+    ctx.imageSmoothingEnabled = false;
+    ctx.webkitImageSmoothingEnabled = false;
+    ctx.mozImageSmoothingEnabled = false;
+    ctx.msImageSmoothingEnabled = false;
+    return ctx;
+  };
+  const initCanvasController = (canvas) => {
+    unref(_canvasController).registerCanvas(canvas);
+  };
+  const initMap = (ctx, width, height) => {
+    axios.get("/api/current_map", { responseType: "blob" }).then((response) => {
+      let img_url = window.URL.createObjectURL(response.data);
+      let img = new Image();
+      img.src = img_url;
+      img.onload = () => {
+        console.log("image loaded");
+        ctx.drawImage(img, 0, 0, width, height);
+      };
+    });
+  };
+  const initSocket = () => {
+    let socket = io("/draw");
+    socket.on("draw_operation", (data) => {
+      drawPoint(unref(ctx), data.x, data.y, Utils.uint32ToRgba(data.color));
+    });
+    return socket;
+  };
+  ctx.value = initCanvas(unref(canvas));
+  initCanvasController(unref(canvas));
+  initMap(unref(ctx), unref(canvas).width, unref(canvas).height);
+  socket.value = initSocket();
+});
+
+const handleClick = (e) => {
+  if (unref(_canvasController).mouse.move) {
+    return;
+  }
+  // 根据缩放获取相对位置
+  let position = unref(_canvasController).getEventRelativePosition(e);
+  let rect = unref(canvas).getBoundingClientRect();
+  let map_scale_ratio = rect.width / size.width;
+  position.x = Math.floor(position.x / map_scale_ratio);
+  position.y = Math.floor(position.y / map_scale_ratio);
+  unref(socket).emit("draw", {
+    x: position.x,
+    y: position.y,
+    color: Utils.rgbaToUint32(unref(props.drawColor)),
+  });
+};
 </script>
 
 <template>
@@ -61,101 +129,3 @@ defineProps({
     </div>
   </div>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      _canvasController: new canvasController(),
-      // 绘制大小
-      size: {
-        width: 1000,
-        height: 1000,
-      },
-      // 画布真实大小
-      map_data: {
-        width: 1000,
-        height: 1000,
-      },
-      // 状态
-    };
-  },
-  mounted() {
-    this.initCanvas();
-    this.initcanvasController();
-    this.initDraw();
-    this.initSocket();
-  },
-  methods: {
-    initCanvas() {
-      let canvas = this.$refs.canvas;
-      this.ctx = canvas.getContext("2d");
-      // 关闭平滑处理以避免像素点模糊
-      this.ctx.imageSmoothingEnabled = false;
-    },
-    initcanvasController() {
-      this._canvasController.registerCanvas(this.$refs.canvas);
-    },
-    // 初始化画布
-    initDraw() {
-      axios
-        .get("/api/current_map", { responseType: "blob" })
-        .then((response) => {
-          let img_url = window.URL.createObjectURL(response.data);
-          let img = new Image();
-          img.src = img_url;
-          img.onload = () => {
-            console.log("image loaded");
-            this.ctx.drawImage(
-              img,
-              0,
-              0,
-              this.$refs.canvas.width,
-              this.$refs.canvas.height
-            );
-          };
-        });
-    },
-    initSocket() {
-      this.socket = io("/draw");
-      this.socket.on("draw_operation", (data) => {
-        // 收到绘制消息
-        this.drawPoint(data.x, data.y, Utils.uint32ToRgba(data.color));
-      });
-    },
-    drawPoint(x, y, color) {
-      const pixel_width = this.size.width / this.map_data.width;
-      const pixel_height = this.size.height / this.map_data.height;
-      this.ctx.fillStyle = color;
-      this.ctx.fillRect(
-        x * pixel_width,
-        y * pixel_height,
-        pixel_width,
-        pixel_height
-      );
-    },
-    handleClick(e) {
-      if (this._canvasController.mouse.move) {
-        return;
-      }
-      // 根据缩放获取相对位置
-      let position = this._canvasController.getEventRelativePosition(e);
-      let rect = this.$refs.canvas.getBoundingClientRect();
-      let map_real_width = rect.width;
-      let map_scale_ratio = map_real_width / this.size.width;
-      position.x = Math.floor(
-        position.x / map_scale_ratio / (this.size.width / this.map_data.width)
-      );
-      position.y = Math.floor(
-        position.y / map_scale_ratio / (this.size.width / this.map_data.width)
-      );
-
-      this.socket.emit("draw", {
-        x: position.x,
-        y: position.y,
-        color: Utils.rgbaToUint32(this.drawColor),
-      });
-    },
-  },
-};
-</script>
-<style scoped></style>
